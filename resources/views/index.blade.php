0bfa0d4bea6959276acf75620879141dae9192b7
sodaler <misha_lector87@mail.ru>
2022-10-12 23:52:15 +0400

    styling main pages

---
+++ resources/views/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-400 text-lg font-semibold">
+                Popular Movies
+            </h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition
+                        ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">
+                            Parasite
+                        </a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="now-playing-movies py-24">
+            <h2 class="uppercase tracking-wider text-orange-400 text-lg font-semibold">
+                Now Playing
+            </h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition
+                        ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">
+                            Parasite
+                        </a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/show.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="movie-info border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
+            <div class="flex-none">
+                <img src="img/parasite.jpg" alt="parasite" class="w-64 lg:w-96">
+            </div>
+            <div class="md:ml-24">
+                <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
+                <div class="flex flex-wrap items-center text-gray-400 text-sm">
+                    <svg class="fill-current text-orange-500 w-4" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+                    </svg>
+                    <span class="ml-1">85%</span>
+                    <span class="mx-2">|</span>
+                    <span>Feb 20, 2020</span>
+                    <span class="mx-2">|</span>
+                    <span>Action, Thriller, Drama</span>
+                </div>
+
+                <p class="text-gray-300 mt-8">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid assumenda consectetur cum
+                    dignissimos earum, enim est ex facilis harum illum libero magnam nobis non nostrum odit perferendis
+                    possimus quae quibusdam quis reprehenderit sapiente sint tempore. Beatae cupiditate dolorem est
+                    exercitationem, ipsum laboriosam nesciunt officia quaerat quas, quia, quidem tempore.
+                </p>
+
+                <div class="mt-12">
+                    <h4 class="text-white font-semibold">Featured Cast</h4>
+                    <div class="flex mt-4">
+                        <div>
+                            <div>Bong Joon-ho</div>
+                            <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
+                        </div>
+                        <div class="ml-8">
+                            <div>Han Jin-won</div>
+                            <div class="text-sm text-gray-400">Screenplay</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-12">
+                    <button class="flex items-center bg-orange-400 text-gray-900 rounded font-semibold px-5
+                    py-4 hover:bg-orange-500 transition ease-in-out duration-150r">
+                        <svg class="w-6 fill-current" width="24" height="24" viewBox="0 0 24 24">
+                            <path
+                                d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+                        </svg>
+                        <span class="ml-2">Play Trailer</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end movie-info -->
+
+    <div class="movie-cast border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">Cast</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite"
+                             class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">
+                            John Smith
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end movie-cast -->
+
+    <div class="movie-images">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">Images</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite"
+                             class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end movie-images -->
+@endsection
